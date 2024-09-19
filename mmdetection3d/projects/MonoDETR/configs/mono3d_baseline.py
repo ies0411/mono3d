c7ab55ab96d9d695771793361a8a5a13664b3d0e
eunsoo.Im <eslim@superb-ai.com>
2024-09-19 20:33:44 +0900

    update

--- mmdetection3d/projects/MonoDETR/configs/mono3d_baseline.py
+++ mmdetection3d/projects/MonoDETR/configs/mono3d_baseline.py
@@ -1,5 +1,5 @@
 _base_ = [
-    "../../../configs/_base_/datasets/nus-3d.py",
+    "../../../configs/_base_/datasets/kitti-3d-3class.py",
     "../../../configs/_base_/default_runtime.py",
     "../../../configs/_base_/schedules/cyclic-20e.py",
 ]
@@ -76,170 +76,170 @@ model = dict(
 )
 # data_root = "data/nuscenes/"
 
-dataset_type = "NuScenesDataset"
-data_root = "/mnt/nas3/Data/nuScenes/v1.0-trainval/"
+# dataset_type = "NuScenesDataset"
+# data_root = "/mnt/nas3/Data/nuScenes/v1.0-trainval/"
 
 backend_args = None
 
-db_sampler = dict(
-    data_root=data_root,
-    info_path=data_root + "nuscenes_dbinfos_train.pkl",
-    rate=1.0,
-    prepare=dict(
-        filter_by_difficulty=[-1],
-        filter_by_min_points=dict(
-            car=5,
-            truck=5,
-            bus=5,
-            trailer=5,
-            construction_vehicle=5,
-            traffic_cone=5,
-            barrier=5,
-            motorcycle=5,
-            bicycle=5,
-            pedestrian=5,
-        ),
-    ),
-    classes=class_names,
-    sample_groups=dict(
-        car=2,
-        truck=3,
-        construction_vehicle=7,
-        bus=4,
-        trailer=6,
-        barrier=2,
-        motorcycle=6,
-        bicycle=6,
-        pedestrian=2,
-        traffic_cone=2,
-    ),
-    points_loader=dict(
-        type="LoadPointsFromFile",
-        coord_type="LIDAR",
-        load_dim=5,
-        use_dim=[0, 1, 2, 3, 4],
-        backend_args=backend_args,
-    ),
-    backend_args=backend_args,
-)
-ida_aug_conf = {
-    "resize_lim": (0.47, 0.625),
-    "final_dim": (320, 800),
-    "bot_pct_lim": (0.0, 0.0),
-    "rot_lim": (0.0, 0.0),
-    "H": 900,
-    "W": 1600,
-    "rand_flip": True,
-}
+# db_sampler = dict(
+#     data_root=data_root,
+#     info_path=data_root + "nuscenes_dbinfos_train.pkl",
+#     rate=1.0,
+#     prepare=dict(
+#         filter_by_difficulty=[-1],
+#         filter_by_min_points=dict(
+#             car=5,
+#             truck=5,
+#             bus=5,
+#             trailer=5,
+#             construction_vehicle=5,
+#             traffic_cone=5,
+#             barrier=5,
+#             motorcycle=5,
+#             bicycle=5,
+#             pedestrian=5,
+#         ),
+#     ),
+#     classes=class_names,
+#     sample_groups=dict(
+#         car=2,
+#         truck=3,
+#         construction_vehicle=7,
+#         bus=4,
+#         trailer=6,
+#         barrier=2,
+#         motorcycle=6,
+#         bicycle=6,
+#         pedestrian=2,
+#         traffic_cone=2,
+#     ),
+#     points_loader=dict(
+#         type="LoadPointsFromFile",
+#         coord_type="LIDAR",
+#         load_dim=5,
+#         use_dim=[0, 1, 2, 3, 4],
+#         backend_args=backend_args,
+#     ),
+#     backend_args=backend_args,
+# )
+# ida_aug_conf = {
+#     "resize_lim": (0.47, 0.625),
+#     "final_dim": (320, 800),
+#     "bot_pct_lim": (0.0, 0.0),
+#     "rot_lim": (0.0, 0.0),
+#     "H": 900,
+#     "W": 1600,
+#     "rand_flip": True,
+# }
 
-train_pipeline = [
-    dict(
-        type="LoadMultiViewImageFromFiles", to_float32=True, backend_args=backend_args
-    ),
-    dict(
-        type="LoadAnnotations3D",
-        with_bbox_3d=True,
-        with_label_3d=True,
-        with_attr_label=False,
-    ),
-    dict(type="ObjectRangeFilter", point_cloud_range=point_cloud_range),
-    dict(type="ObjectNameFilter", classes=class_names),
-    # dict(type="ResizeCropFlipImage", data_aug_conf=ida_aug_conf, training=True),
-    # dict(
-    #     type="GlobalRotScaleTransImage",
-    #     rot_range=[-0.3925, 0.3925],
-    #     translation_std=[0, 0, 0],
-    #     scale_ratio_range=[0.95, 1.05],
-    #     reverse_angle=False,
-    #     training=True,
-    # ),
-    dict(
-        type="Pack3DDetInputs",
-        keys=[
-            "img",
-            "gt_bboxes",
-            "gt_bboxes_labels",
-            "attr_labels",
-            "gt_bboxes_3d",
-            "gt_labels_3d",
-            "centers_2d",
-            "depths",
-        ],
-    ),
-]
-test_pipeline = [
-    dict(
-        type="LoadMultiViewImageFromFiles", to_float32=True, backend_args=backend_args
-    ),
-    dict(type="ResizeCropFlipImage", data_aug_conf=ida_aug_conf, training=False),
-    dict(type="Pack3DDetInputs", keys=["img"]),
-]
+# train_pipeline = [
+#     dict(
+#         type="LoadMultiViewImageFromFiles", to_float32=True, backend_args=backend_args
+#     ),
+#     dict(
+#         type="LoadAnnotations3D",
+#         with_bbox_3d=True,
+#         with_label_3d=True,
+#         with_attr_label=False,
+#     ),
+#     dict(type="ObjectRangeFilter", point_cloud_range=point_cloud_range),
+#     dict(type="ObjectNameFilter", classes=class_names),
+# dict(type="ResizeCropFlipImage", data_aug_conf=ida_aug_conf, training=True),
+# dict(
+#     type="GlobalRotScaleTransImage",
+#     rot_range=[-0.3925, 0.3925],
+#     translation_std=[0, 0, 0],
+#     scale_ratio_range=[0.95, 1.05],
+#     reverse_angle=False,
+#     training=True,
+# ),
+#     dict(
+#         type="Pack3DDetInputs",
+#         keys=[
+#             "img",
+#             "gt_bboxes",
+#             "gt_bboxes_labels",
+#             "attr_labels",
+#             "gt_bboxes_3d",
+#             "gt_labels_3d",
+#             "centers_2d",
+#             "depths",
+#         ],
+#     ),
+# ]
+# test_pipeline = [
+#     dict(
+#         type="LoadMultiViewImageFromFiles", to_float32=True, backend_args=backend_args
+#     ),
+#     dict(type="ResizeCropFlipImage", data_aug_conf=ida_aug_conf, training=False),
+#     dict(type="Pack3DDetInputs", keys=["img"]),
+# ]
 
-train_dataloader = dict(
-    batch_size=1,
-    num_workers=4,
-    dataset=dict(
-        type=dataset_type,
-        data_prefix=dict(
-            pts="samples/LIDAR_TOP",
-            CAM_FRONT="samples/CAM_FRONT",
-            CAM_FRONT_LEFT="samples/CAM_FRONT_LEFT",
-            CAM_FRONT_RIGHT="samples/CAM_FRONT_RIGHT",
-            CAM_BACK="samples/CAM_BACK",
-            CAM_BACK_RIGHT="samples/CAM_BACK_RIGHT",
-            CAM_BACK_LEFT="samples/CAM_BACK_LEFT",
-        ),
-        pipeline=train_pipeline,
-        box_type_3d="LiDAR",
-        metainfo=metainfo,
-        test_mode=False,
-        modality=input_modality,
-        use_valid_flag=True,
-        backend_args=backend_args,
-    ),
-)
-test_dataloader = dict(
-    dataset=dict(
-        type=dataset_type,
-        data_prefix=dict(
-            pts="samples/LIDAR_TOP",
-            CAM_FRONT="samples/CAM_FRONT",
-            CAM_FRONT_LEFT="samples/CAM_FRONT_LEFT",
-            CAM_FRONT_RIGHT="samples/CAM_FRONT_RIGHT",
-            CAM_BACK="samples/CAM_BACK",
-            CAM_BACK_RIGHT="samples/CAM_BACK_RIGHT",
-            CAM_BACK_LEFT="samples/CAM_BACK_LEFT",
-        ),
-        pipeline=test_pipeline,
-        box_type_3d="LiDAR",
-        metainfo=metainfo,
-        test_mode=True,
-        modality=input_modality,
-        use_valid_flag=True,
-        backend_args=backend_args,
-    )
-)
-val_dataloader = dict(
-    dataset=dict(
-        type=dataset_type,
-        data_prefix=dict(
-            pts="samples/LIDAR_TOP",
-            CAM_FRONT="samples/CAM_FRONT",
-            CAM_FRONT_LEFT="samples/CAM_FRONT_LEFT",
-            CAM_FRONT_RIGHT="samples/CAM_FRONT_RIGHT",
-            CAM_BACK="samples/CAM_BACK",
-            CAM_BACK_RIGHT="samples/CAM_BACK_RIGHT",
-            CAM_BACK_LEFT="samples/CAM_BACK_LEFT",
-        ),
-        pipeline=test_pipeline,
-        box_type_3d="LiDAR",
-        metainfo=metainfo,
-        test_mode=True,
-        modality=input_modality,
-        use_valid_flag=True,
-        backend_args=backend_args,
-    )
-)
+# train_dataloader = dict(
+#     batch_size=1,
+#     num_workers=4,
+#     dataset=dict(
+#         type=dataset_type,
+#         data_prefix=dict(
+#             pts="samples/LIDAR_TOP",
+#             CAM_FRONT="samples/CAM_FRONT",
+#             CAM_FRONT_LEFT="samples/CAM_FRONT_LEFT",
+#             CAM_FRONT_RIGHT="samples/CAM_FRONT_RIGHT",
+#             CAM_BACK="samples/CAM_BACK",
+#             CAM_BACK_RIGHT="samples/CAM_BACK_RIGHT",
+#             CAM_BACK_LEFT="samples/CAM_BACK_LEFT",
+#         ),
+#         pipeline=train_pipeline,
+#         box_type_3d="LiDAR",
+#         metainfo=metainfo,
+#         test_mode=False,
+#         modality=input_modality,
+#         use_valid_flag=True,
+#         backend_args=backend_args,
+#     ),
+# )
+# test_dataloader = dict(
+#     dataset=dict(
+#         type=dataset_type,
+#         data_prefix=dict(
+#             pts="samples/LIDAR_TOP",
+#             CAM_FRONT="samples/CAM_FRONT",
+#             CAM_FRONT_LEFT="samples/CAM_FRONT_LEFT",
+#             CAM_FRONT_RIGHT="samples/CAM_FRONT_RIGHT",
+#             CAM_BACK="samples/CAM_BACK",
+#             CAM_BACK_RIGHT="samples/CAM_BACK_RIGHT",
+#             CAM_BACK_LEFT="samples/CAM_BACK_LEFT",
+#         ),
+#         pipeline=test_pipeline,
+#         box_type_3d="LiDAR",
+#         metainfo=metainfo,
+#         test_mode=True,
+#         modality=input_modality,
+#         use_valid_flag=True,
+#         backend_args=backend_args,
+#     )
+# )
+# val_dataloader = dict(
+#     dataset=dict(
+#         type=dataset_type,
+#         data_prefix=dict(
+#             pts="samples/LIDAR_TOP",
+#             CAM_FRONT="samples/CAM_FRONT",
+#             CAM_FRONT_LEFT="samples/CAM_FRONT_LEFT",
+#             CAM_FRONT_RIGHT="samples/CAM_FRONT_RIGHT",
+#             CAM_BACK="samples/CAM_BACK",
+#             CAM_BACK_RIGHT="samples/CAM_BACK_RIGHT",
+#             CAM_BACK_LEFT="samples/CAM_BACK_LEFT",
+#         ),
+#         pipeline=test_pipeline,
+#         box_type_3d="LiDAR",
+#         metainfo=metainfo,
+#         test_mode=True,
+#         modality=input_modality,
+#         use_valid_flag=True,
+#         backend_args=backend_args,
+#     )
+# )
 
 # Different from original PETR:
 # We don't use special lr for image_backbone
